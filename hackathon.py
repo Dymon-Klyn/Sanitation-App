78dd0e1de8268f19a58d5e49f8745663cd09f17d
navarroalan55 <navarroalan55@gmail.com>
2020-06-06 08:14:54 -0400

    updated GUI

--- hackathon.py
+++ hackathon.py
@@ -1,12 +1,11 @@
 # Tool to reenter society after COVID-19
-# Make a GUI
-# Make a nice GUI a very nice GUI
 
 # We need a JSON File to keep track of the lists
 import json
 import ctypes
 import tkinter as tk
 from tkinter import simpledialog
+from tkinter import messagebox
 
 # Look of the GUI
 """
@@ -57,6 +56,7 @@ def remove():
     listbox.delete(listbox.index("active"))
     move_data(data, event, "events", "completed")
     save_data(data)
+    messagebox.showinfo("Reminder", "Please remember to wear a mask and wash your hands now that you are completed with the task!")
     #ctypes.windll.user32.MessageBoxW(0, "Please remember to wear a mask and wash your hands now that you are completed with the task!", "Reminder", 0)
     listbox1.insert("end", event)
 
@@ -80,21 +80,23 @@ main = tk.Frame(root, width = 70, height = 400 )
 
 # Welcome Frame
 welcome_frame = tk.Frame(main, width = 70, height = 60)
-welcome_label = tk.Label(welcome_frame, text = "WELCOME BACK!", font = ("Times New Roman", 50))
+welcome_label = tk.Label(welcome_frame, text = "      WELCOME BACK!", font = ("Times New Roman", 50))
 
 spacer1 = tk.Frame(main, width = 70, height = 40)
 upcoming_label = tk.Label(spacer1, text = "Upcoming Events:", font = ("Times New Roman", 15))
 
 # Upcoming Frame
 upcoming_frame = tk.Frame(main, width = 70, height = 50)
-scrollbar = tk.Scrollbar(upcoming_frame, orient="vertical")
-listbox = tk.Listbox(upcoming_frame, width = 80, height = 5, yscrollcommand=scrollbar.set)
+upcoming_frame1 = tk.Frame(upcoming_frame, width = 20, height = 50)
+upcoming_frame2 = tk.Frame(upcoming_frame, width = 50, height = 50)
+scrollbar = tk.Scrollbar(upcoming_frame2, orient="vertical")
+listbox = tk.Listbox(upcoming_frame2, width = 80, height = 5, yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 for item in data["events"]:
     listbox.insert("end", item)
 
 
-listbox_remove_butt = tk.Button(upcoming_frame, text = "Complete Task",
+listbox_remove_butt = tk.Button(upcoming_frame1, text = "Complete Task",
 font = ("Times New Roman", 12), command = remove)
 
 
@@ -103,14 +105,16 @@ events_label = tk.Label(spacer2, text = "Events Completed:", font = ("Times New 
 
 # Events Frame
 events_frame = tk.Frame(main, width = 70, height = 50)
-scrollbar1 = tk.Scrollbar(events_frame, orient="vertical")
-listbox1 = tk.Listbox(events_frame, width = 80, height = 5, yscrollcommand=scrollbar.set)
+events_frame1 = tk.Frame(events_frame, width = 20, height = 50)
+events_frame2 = tk.Frame(events_frame, width = 50, height = 50)
+scrollbar1 = tk.Scrollbar(events_frame2, orient="vertical")
+listbox1 = tk.Listbox(events_frame2, width = 80, height = 5, yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 for item in data["completed"]:
     listbox1.insert("end", item)
 
-listbox1_remove_butt = tk.Button(events_frame, text = "Clear",
-font = ("Times New Roman", 12), command = clear)
+listbox1_remove_butt = tk.Button(events_frame1, text = "Clear",
+font = ("Times New Roman", 12), command = clear, width = 12)
 
 
 spacer3 = tk.Frame(main, width = 70, height = 50)
@@ -123,8 +127,8 @@ curfew_frame = tk.Frame(main, width = 70, height = 50)
 
 # Create Frame
 create_frame = tk.Frame(main, width = 70, height = 40)
-create_butt = tk.Button(create_frame, text = "Create New                                                                   +",
-font = ("Times New Roman", 25), command = create)
+create_butt = tk.Button(create_frame, text = "Create New Event                                             +",
+font = ("Times New Roman", 25), command = create, width = 40)
 
 
 # Place all widgets on screen
@@ -137,18 +141,22 @@ spacer1.pack(fill = "x")
 upcoming_label.grid(row = 0, column = 0)
 
 upcoming_frame.pack(fill = "x")
+upcoming_frame1.grid(row = 0, column = 0)
+upcoming_frame2.grid(row = 0, column = 1)
 listbox.grid(row = 0, column = 0)
-scrollbar.grid(row = 0, column = 1)
-listbox_remove_butt.grid(row = 0, column = 2)
+scrollbar.grid(row = 0, column = 1, ipady = 28 )
+listbox_remove_butt.grid(row = 0, column = 2, padx = (0, 10))
 
 
 spacer2.pack(fill = "x")
 events_label.grid(row = 0, column = 0)
 
 events_frame.pack(fill = "x")
+events_frame1.grid(row = 0, column = 0)
+events_frame2.grid(row = 0, column = 1)
 listbox1.grid(row = 0, column = 0)
-scrollbar1.grid(row = 0, column = 1)
-listbox1_remove_butt.grid(row = 0, column = 2)
+scrollbar1.grid(row = 0, column = 1, ipady = 28)
+listbox1_remove_butt.grid(row = 0, column = 2, padx = (0, 10))
 
 spacer3.pack(fill = "x")
 curfew_label.grid(row = 0, column = 0)
@@ -162,4 +170,3 @@ create_butt.grid(row = 0, column = 0)
 
 
 root.mainloop()
-
