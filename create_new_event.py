1ad470b8614362e1eb2cda1623e860b75522a766
MarioIsntFunny <44925876+MarioIsntFunny@users.noreply.github.com>
2020-06-06 00:28:55 -0400

    Delete create_new_event.py

--- create_new_event.py
+++
@@ -1,8 +0,0 @@
-def create():
-    events = []
-    i = 0
-    choice = "Y"
-    count = int(simpledialog.askstring("Number of Events", "How many events would you like to create?", parent=main))
-    for i in range(count):
-        events.append([simpledialog.askstring("Input", "New Task", parent= main)])
-    return [events]
